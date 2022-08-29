fcba2e886f92127cda0c2361d7bd8958eb74ac82
229895 <229895@edu.p.lodz.pl>
2022-08-29 23:42:42 +0200

    posts imported, working on proper display

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'Test 1', excerpt: 'test excerpt etst excerpt' },
-  { title: 'Test 2', excerpt: 'test excerpt etst excerpt' }
-];
 
-export default function Home() {
+
+export default function Home({posts}) {
   return (
     <div className='container mx-auto px-10 mb-8'>
 
@@ -18,8 +16,8 @@ export default function Home() {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
 
         <div className='lg:col-span-8 cols-span-1'>
-          {posts.map((post, index) => (
-            <PostCard post={post} key={post.title}/>
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title}/>
           ))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
@@ -33,4 +31,12 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
