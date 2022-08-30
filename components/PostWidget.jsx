4e2a4a549c06c3bc29d83184349330088a87a860
229895 <229895@edu.p.lodz.pl>
2022-08-30 23:53:56 +0200

    post details completed

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import moment from "moment";
 import Link from "next/link";
 
-import { getRecentPosts, getSimiliarPosts } from "../services";
+import { getRecentPosts, getSimilarPosts } from "../services";
 
 const PostWidget = ({categories, slug}) => {
     const [relatedPosts, setRelatedPosts] = useState([]);
--- services/index.js
+++ services/index.js
@@ -40,6 +40,40 @@ export const getPosts = async () => {
     return result.postsConnection.edges;
   }
 
+  export const getPostDetails = async (slug) => {
+    const query = gql`
+      query GetPostDetails($slug : String!) {
+        post(where: {slug: $slug}) {
+          title
+          excerpt
+          featuredImage {
+            url
+          }
+          author {
+            name
+            bio
+            photo {
+              url
+            }
+          }
+          createdAt
+          slug
+          content {
+            raw
+          }
+          categories {
+            name
+            slug
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.post;
+  };
+
   export const getRecentPosts = async () => {
     const query = gql`
       query GetPostDetails() {
@@ -62,7 +96,7 @@ export const getPosts = async () => {
     return result.posts;
   }
 
-  export const getSimiliarPosts = async () => {
+  export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
       query GetPostDetails($slug: String!, $categories: [String!]) {
         posts(
@@ -79,7 +113,7 @@ export const getPosts = async () => {
       }
     `
 
-    const result = await request(graphqlAPI, query);
+    const result = await request(graphqlAPI, query, { categories, slug });
 
     return result.posts;
   }
