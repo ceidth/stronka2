220baf4e494d733527c218a222d954cf61c58f79
229895 <229895@edu.p.lodz.pl>
2022-08-31 20:29:37 +0200

    blog finished

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>Author</div>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-orange-200 bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image 
+          src={author.photo.url}
+          alt={author.name}
+          unoptimized
+          height="100px"
+          width="100px"
+          className="align-middle rounded-full" />
+      </div>
+      <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-ls">{author.bio}</p>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services'
-
+import {FeaturedPosts} from '../sections'
 
 
 export default function Home({posts}) {
@@ -12,7 +12,7 @@ export default function Home({posts}) {
         <title>Deeper</title>
         <link rel="icon" href='/favicon.ico'/>
       </Head>
-
+      <FeaturedPosts/>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
 
         <div className='lg:col-span-8 cols-span-1'>
